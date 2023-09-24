2ac6209ca6ad77c84bd2b10e21138dc1985b83ab
Shah Danish <shah.official1011@gmail.com>
2023-09-24 19:50:28 +0530

    Posts and forms to the client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import memories from './images/memories.png';
+import  Form  from './components/Form/Form';
+import  Posts  from './components/Posts/Posts';
+
 
 const App = () => {
   return (
-    <div>
-        <h1>
-            Hello World
-        </h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color='inherit'>
+        <Typography variant='h2' align='center'>
+            Memoria
+        </Typography>
+        <img src={memories} alt='Memoria' height="60"/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            </Grid>
+            <Grid item xs={12} sm={7}>
+              <Form/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
