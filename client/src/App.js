8432871ee873cff2d33dcbc4b19190556b31dcba
Shah Danish <shah.official1011@gmail.com>
2023-09-25 22:46:31 +0530

    Removing material UI styles from Appjs
    Installed TailwindCSS in Client

--- client/src/App.js
+++ client/src/App.js
@@ -5,28 +5,13 @@ import  Form  from './components/Form/Form';
 import  Posts  from './components/Posts/Posts';
 
 
+
 const App = () => {
+
   return (
-    <Container maxWidth="lg">
-      <AppBar position="static" color='inherit'>
-        <Typography variant='h2' align='center'>
-            Memoria
-        </Typography>
-        <img src={memories} alt='Memoria' height="60"/>
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container justify='space-between' alignItems='stretch' spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts/>
-            </Grid>
-            <Grid item xs={12} sm={7}>
-              <Form/>
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <h1>
+      Hello World!
+    </h1>
   )
 }
 
