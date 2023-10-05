53c785428a6afded21e0b7289b3829e94cbba7aa
Shah Danish <shah.official1011@gmail.com>
2023-10-05 20:28:02 +0530

    Navbar implemented
    issue in hamburger menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import  Form  from './components/Form/Form';
 import  Posts  from './components/Posts/Posts';
+import NavLink from './components/NavLink/NavLink'
 import { useDispatch } from 'react-redux';
-import { getPosts } from './actions/posts'
+import { getPosts } from './actions/posts';
+import Landing from './pages/Landing';
 
 const App = () => {
 const [currentId, setCurrentId] = useState(null);
@@ -14,10 +16,9 @@ useEffect(() =>{
 
   return (
     <main className='bg-gray-900' >
-      <div className='flex justify-center px-4'>
-        <h1 className='text-[50px] font-montserrat text-blue-500'>Memoria</h1>
-      </div>
-      <div className='flex justify-around py-4 max-sm:flex-col-reverse'>
+      <NavLink/>
+      <Landing />
+      <div className='flex justify-around py-[40px] max-sm:flex-col-reverse'>
         <Posts setCurrentId={setCurrentId} />
         <Form currentId={currentId} setCurrentId={setCurrentId} />
       </div>
---
+++ client/src/components/NavLink/NavLink.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+const NavLink = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
+  return (
+      <nav className=" w-full z-10 flex py-[10px] px-[80px] justify-between items-center max-container bg-gray-800 max-sm:p-10 bg-opacity-80 border border-gray-800" style={{ backdropFilter: "blur(20px)" }}>
+        <a href="/">
+          <h1 className="text-[35px] text-blue-500">Memoria</h1>
+        </a>
+
+        <div className='flex gap-10 items-center font-medium font-montserrat max-lg:hidden wide:mr-24'>
+        <div className="text-blue-500 text-xl leading-normal hover:text-white">
+          <a href='/'>Log in</a>
+        </div>
+        <div className="text-gray-800 text-xl bg-blue-500 p-[8px] rounded-md leading-normal hover:text-white">
+          <a href='/'>Sign up</a>
+        </div>
+        </div>
+        
+        {/* This is for the hamburger menu 👇🏼*/}
+
+        <button
+          onClick={toggleMenu}
+          className="lg:hidden focus:outline-none"
+          aria-label="Toggle Menu"
+          style={{ zIndex: 1000 }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="white"
+            className="w-8 h-8"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"
+            />
+          </svg>
+        </button>
+        <ul
+          className={`${
+            menuVisible ? "block" : "hidden"
+          } lg:hidden absolute top-[6rem] right-5 bg-gray-900 py-2 rounded-md` }
+        >
+          
+          <div className="flex flex-col  leading-normal font-medium font-montserrat rounded-md text-xl">
+            <a className="mx-[30px] text-blue-500 py-2 px-2 " href="/">
+              Log in 
+            </a>
+            <a className="mx-[30px] text-blue-500 py-2 px-2 " href="/">
+              Sign Up
+            </a>
+          </div>
+        </ul>
+      </nav>
+  );
+};
+
+export default NavLink;
+
+
+
+
+
