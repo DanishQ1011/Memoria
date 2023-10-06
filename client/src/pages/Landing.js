041d1fe18f2dcf9551dbc90dc042beacdd6e0808
Shah Danish <shah.official1011@gmail.com>
2023-10-06 23:07:39 +0530

    Minor update to sign up form

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import landingpage from '../images/landingpage.jpg';
 
+
 const Landing = () => {
+
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
   return (
     <div className="flex h-screen bg-gray-900">
       <div className="w-1/2 bg-blue-500">
         <img src= {landingpage} />
       </div>
-      <div className="w-1/2 py-[100px]">
+      <div className="w-1/2 ">
         <div className='flex justify-center'>
         <form className='max-w-[1440px] '>
       <div className='flex max-sm:justify-center '>
@@ -62,14 +70,33 @@ const Landing = () => {
                       </div>
                       <div>
                         <label htmlFor="password" className="block mb-1 text-sm sm:mb-2 sm:text-base font-medium text-gray-900 dark:text-white">Password</label>
+                        <div className='flex'>
                         <input
-                          type='password'
+                          type={passwordVisible ? 'text' : 'password'}
                           name="password"
                           label="password"
                           className="block w-full p-2 text-sm sm:text-base text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                           placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;"
                           required
                         />
+                        <button
+                                type="button"
+                                onClick={togglePasswordVisibility}
+                                className="absolute inset-y-0 right-0 mr-8 text-gray-400 hover:text-gray-600 cursor-pointer"
+                              >
+                                {passwordVisible ? (
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+                                  </svg>
+                                ) : (
+                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                  </svg>
+                                )}
+                        </button>
+                        
+                      </div> 
                       </div>
                       <div>
                         <label htmlFor="password" className="block mb-1 text-sm sm:mb-2 sm:text-base font-medium text-gray-900 dark:text-white">Repeat password</label>
@@ -102,6 +129,9 @@ const Landing = () => {
                         </svg>
                         Continue with Google
                       </button>
+                    </div>
+                    <div>
+                      <h1 className='text-center'>Already have an account? <span className='text-blue-500 cursor-pointer hover:text-blue-300'>Log in</span> </h1>
                     </div>
                     </div>
                 </div>
